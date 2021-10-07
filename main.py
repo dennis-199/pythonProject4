8fc6492d6b962bc47f3b3aa84d548ff920838d8c
dennis-199 <otieno.dennis@strathmore.edu>
2021-10-07 16:03:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import turtle
+wn = turtle.Screen()
+dennis = turtle.Turtle()
+distance = 50
+angle = 90
+for _ in range(10):
+    dennis.forward(distance)
+    dennis.right(angle)
+    distance=distance+10
+
+
